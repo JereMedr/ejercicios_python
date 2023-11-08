ab9bd0ea1ade556a874175d8f903ca59cb3fae49
JereMedr <jeremedrano228@gmail.com>
2023-11-08 12:46:37 -0300

    cambios en el readme, instrucciones de uso de git y cambios en el ejecicio operadores

--- src/operadores/operadores.py
+++ src/operadores/operadores.py
@@ -1,11 +1,14 @@
 #ejercicio de operadores con python con numeros
 
-#1. sumar dos numeros y mostrar el resultado
-#2. restar dos numeros y mostrar el resultado
-#3. multiplicar dos numeros y mostrar el resultado
-#4. dividir dos numeros y mostrar el resultado
-#5. mostrar el resto de dos numeros
-#6. mostrar la division entera de dos numeros
-#7. elevar un numero a otro numero
-#8. mostrar el resultado de la raiz cuadrada de un numero
-#9. mostrar el resultado de la raiz cubica de un numero
+#1. crear dos variables de tipo entero y sumarlas, asignar el resultado a una tercera variable e imprimir el resultado
+#2. crear dos variables de tipo entero y restarlas, asignar el resultado a una tercera variable e imprimir el resultado
+#3. crear dos variables de tipo entero y multiplicarlas, asignar el resultado a una tercera variable e imprimir el resultado
+#4. crear dos variables de tipo entero y dividirlas, asignar el resultado a una tercera variable e imprimir el resultado
+#5. crear dos variables de tipo entero y elevar la primera a la potencia de la segunda, asignar el resultado a una tercera variable e imprimir el resultado
+#6. crear dos variables de tipo entero y mostrar el resto de la división entre ellas, asignar el resultado a una tercera variable e imprimir el resultado
+
+# ejemplo 1
+a = 10
+b = 20
+resultado = a + b
+print(resultado)
