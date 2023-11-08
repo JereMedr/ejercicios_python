4aca1b6f9fb2e1635691185c896c05911bbc7338
tommy <parajuegosdou@gmail.com>
2023-11-08 16:41:56 -0300

    Ejercicio Resuelto con pequeña aclaracion al final

--- src/operadores/operadores.py
+++ src/operadores/operadores.py
@@ -8,7 +8,40 @@
 #6. crear dos variables de tipo entero y mostrar el resto de la división entre ellas, asignar el resultado a una tercera variable e imprimir el resultado
 
 # ejemplo 1
+print ("Ejercicio 1")
 a = 10
 b = 20
 resultado = a + b
 print(resultado)
+
+# Ejercicio 2 
+print ("Ejercicio 2")
+a = 52
+b = 24
+resultado = a - b
+print(resultado)
+
+# Ejercicio 3 
+print ("Ejercicio 3")
+a = 2
+b = 8
+resultado = a * b
+print(resultado)
+# Ejercicio 4
+print ("Ejercicio 4")
+a = 9
+b = 3
+resultado = a / b
+print(resultado) 
+# Ejercicio 5
+print ("Ejercicio 5")
+a = 7
+b = 2
+resultado = a ** b
+print(resultado) 
+# Ejercicio 6
+print ("Ejercicio 6")
+numero1 = 10
+numero2 = 3 
+resultado = numero1 % numero2
+print("El resto de la división de", numero1, "entre", numero2, "es:", resultado)
