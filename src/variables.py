8d16b89e968adbd24f065f2844c64fc72d3c99ba
JereMedr <jeremedrano228@gmail.com>
2023-11-08 10:17:14 -0300

    ejercicio resuelto de variables

--- src/variables.py
+++ src/variables.py
@@ -1,2 +1,45 @@
 # Crear variables de tipo string, int, float, boolean, list, tuple, dict, set
 # y mostrar el tipo de dato de cada variable con la función type()
+
+#String
+name = "Jeremias Medrano"
+print(name)
+print(type(name))
+
+#int
+age = 29
+print(age)
+print(type(age))
+
+#float
+height = 1.70
+print(height)
+print(type(height))
+
+#boolean
+is_adult = True
+print(is_adult)
+print(type(is_adult))
+
+#list
+languages = ["Python", "JavaScript", "Java"]
+print(languages)
+print(type(languages))
+
+#tuple
+frameworks = ("Django", "React", "Spring")
+print(frameworks)
+print(type(frameworks))
+
+# dict
+dbms = {"MySQL": "Relacional", 
+        "MongoDB": "No Relacional"}
+print(dbms)
+print(type(dbms))
+
+#set
+tools = {"PyCharm", "VSCode", "IntelliJ"}
+print(tools)
+print(type(tools))
+
+# Path: src/variables.py
