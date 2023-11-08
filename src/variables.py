f017036b3536f388d3d57d3d4d6d566f4afb48d9
JereMedr <jeremedrano228@gmail.com>
2023-11-08 10:21:17 -0300

    se pone un ejemplo de lo que se quiere escribir

--- src/variables.py
+++ src/variables.py
@@ -1,2 +1,8 @@
 # Crear variables de tipo string, int, float, boolean, list, tuple, dict, set
 # y mostrar el tipo de dato de cada variable con la función type()
+
+#ejemplo
+#string
+name = "Juan"
+print(name)
+print(type(name))
