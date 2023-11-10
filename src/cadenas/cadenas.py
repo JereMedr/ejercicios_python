fceca299844e4632c547cf97f996fd157adc91ce
JereMedr <jeremedrano228@gmail.com>
2023-11-09 21:39:19 -0300

    se agrego ejercicio de cadenas en el directorio src

---
+++ src/cadenas/cadenas.py
@@ -0,0 +1,16 @@
+# crear una variable nombre de tipo string con el valor inicial "Python" a la variable nombre:
+# 1) ponerla en mayusculas y mostrar en pantalla
+# 2) ponerla en minusculas y mostrar en pantalla
+# 3) mostrar la longitud de la cadena
+# 4) mostrar la primera letra de la cadena
+# 5) mostrar la ultima letra de la cadena
+# 6) mostrar la letra que esta en la posicion 3 de la cadena
+# 7) mostrar la cadena en orden inverso
+# 8) mostrar la cadena en orden inverso pero sin la primera letra
+# 9) mostrar la cadena en orden inverso pero sin la ultima letra
+# 10) mostrar la cadena en orden inverso pero sin la primera ni la ultima letra
+# 11) mostrar la cadena en orden inverso pero solo los 3 ultimos caracteres
+# 12) mostrar la cadena en orden inverso pero solo los 3 primeros caracteres
+# 13) mostrar la cadena en orden inverso pero solo los 3 ultimos y los 3 primeros caracteres pero en mayusculas
+# 14) concatenar la cadena con la cadena " es un lenguaje de programacion"
+# 15) concatenar la cadena con la cadena " es un lenguaje de programacion" y mostrar la longitud de la cadena resultante
