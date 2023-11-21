2cfff8bbdbbdefd8714c3e49b9875017583f94a2
JereMedr <jeremedrano228@gmail.com>
2023-11-20 21:10:54 -0300

    ejercicio nuevo de condicionales

---
+++ src/condicionales/condicionales.py
@@ -0,0 +1,14 @@
+# Escribir 4 condionales a modo de ejemplo para los siguientes problemas:
+
+# 1. Tengo la variable edad = 18, si la edad es mayor o igual a 18 imprimir "Es mayor de edad" sino imprimir "Es menor de edad"
+# 2. Tengo la variable numero = 10, si el numero es par imprimir "Es par" sino imprimir "Es impar"
+# 3. Tengo la variable numero = 10, si el numero es mayor a 10 imprimir "Es mayor a 10" sino imprimir "Es menor o igual a 10"
+# 4. tengo la variable nombre = "Juan", si el nombre es igual a "Juan" imprimir "Hola Juan" sino imprimir "Hola"
+# 5. Tengo la variable lista_numeros = [1,2,3,4,5], si el numero 3 esta en la lista imprimir "El numero 3 esta en la lista" sino imprimir "El numero 3 no esta en la lista"
+
+#ejercicio de ejemplo 3
+numero =  11
+if numero > 10:
+    print("Es mayor a 10")
+else:
+    print("Es menor o igual a 10")
