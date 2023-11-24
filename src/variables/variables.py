4ae608a540f580d782d1879d829566d3994d6671
tommy <parajuegosdou@gmail.com>
2023-11-24 12:19:12 -0300

    Ejercicios variables completado

--- src/variables/variables.py
+++ src/variables/variables.py
@@ -1,8 +1,33 @@
+
 # Crear variables de tipo string, int, float, boolean, list, tuple, dict, set
 # y mostrar el tipo de dato de cada variable con la función type()
 
-#ejemplo
-#string
-name = "Trueno"
+# Ejemplo de string
+name = "Andres Calamaro"
 print(name)
 print(type(name))
+
+# Ejemplo de entero (int)
+age = 35
+print(age)
+print(type(age))
+
+# Ejemplo de flotante (float)
+height = 1.75
+print(height)
+print(type(height))
+
+# Ejemplo de booleano (boolean)
+is_student = True
+print(is_student)
+print(type(is_student))
+
+# Ejemplo de lista (list)
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(type(fruits))
+
+# Ejemplo de tupla (tuple)
+coordinates = (40.7128, -74.0060)
+print(coordinates)
+print(type(coordinates))
