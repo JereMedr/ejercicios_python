a473ad2a056e68e77ac66996831b2bcd4f947b38
JereMedr <jeremedrano228@gmail.com>
2023-12-11 10:14:51 -0300

    ciclos ejercicio

---
+++ src/ciclos/ciclos.py
@@ -0,0 +1,42 @@
+#Los ciclos :
+# En el contexto de programación en Python, un "ciclo" se refiere a 
+# una estructura que permite ejecutar un bloque de código varias veces.  
+# Los ciclos son fundamentales en programación para automatizar tareas repetitivas. En Python, 
+# hay dos tipos principales de ciclos: el bucle for y el bucle while
+
+# 1) crear una lista con los numeros del 1 al 10 recorrer la lista y por cada elemento de la lista mostrar el elemento con un print con el siguiente mensaje 
+# print("El numero del indice 'n' tiene el valor de : ",n)
+# list = [1,2,3,4,5,6,7,8,9,10]
+# for n in list:
+#     print("El numero del indice 'n' tiene el valor de : ",n)
+
+#2) crear una lista con los numeros del 1 al 10 recorrer la lista y por cada elemento de la lista mostrar el elemento multiplicado por dos  con un print con 
+# el siguiente mensaje: ("El numero del indice 'n' tiene el valor de : ",n)
+
+# 3) crear una lista con nombres de frutas los nombres de frutas deben empezar en mayuscula, recorrer la lista y por cada elemento de la lista mostrar el mensaje
+# "Me gusta comer " + fruta
+
+# 4) crear una lista con nombres de frutas los nombres de frutas deben empezar en mayuscula, recorrer la lista y por cada elemento de la lista mostrar el elemento en 
+# mayusculas y el siguiente mensaje: "ME GUSTA COMER " + fruta
+
+# 5 ) Crear una lista de numeros del 1 al 20 , recorrer la lista y solo mostrarlo en pantalla si el numero es par (usar condicional dentro del for para evaluar si el numero es par)
+
+# 6) crear una lista usando un while con los numeros del 1 al 10 recorrer la lista y por cada elemento de la lista mostrar el elemento con un print con el siguiente mensaje
+# print("El numero del indice 'n' tiene el valor de : ",n)
+n = 0
+while n < 10:
+    print("El numero del indice 'n' tiene el valor de : ",n)
+    n += 1
+
+# 7) crear una lista usando un while con los numeros del 1 al 10 recorrer la lista y por cada elemento de la lista mostrar el elemento multiplicado por dos  con un print con
+# el siguiente mensaje: ("El numero del indice 'n' tiene el valor de : ",n)
+
+# 8) hacer una cuenta regresiva desde el 10 hasta el 0 usando un while
+
+# 9) hacer una cuenta regresiva desde el 10 hasta el 0 usando un for
+
+# 10)  crear una lista con numeros del 1 al 100 con un while, a esa lista recorrerla con un for y solo mostrar los elementos que sean multiplos de 7 con el siguiente mensaje:
+# "El numero"+ n + "es multiplo de 7"
+
+
+
